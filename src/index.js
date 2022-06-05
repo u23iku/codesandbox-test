cd811ccadb166e9d55ac00009ccea4bffc473516
u23iku <106704490+u23iku@users.noreply.github.com>
2022-06-05 15:28:09 +0900

    アロー関数まで実施

--- src/index.js
+++ src/index.js
@@ -40,6 +40,43 @@
 // console.log(val4);
 
 //constで定義した配列はプロパティの変更が可能
-const val5 = ["dog", "cat"];
-val5.push("monkey");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = "ikue";
+// const age = 38;
+//「私のなまえはikueです。年齢は38歳です。」
+
+//従来の方法
+// const message1 = "私のなまえは" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message1);
+
+/**
+ * アロー関数
+ */
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// // アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
+
+// const func3 = (num1, num2) => num1 + num2;
+
+// console.log(func3(10, 20));
